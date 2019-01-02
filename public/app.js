03b8a87429ef6efdb447e27fd77aff6ccb4e4849
ricky <ricky@rickys-MacBook-Air.local>
2019-01-02 13:54:02 -0800

    deploying to github

--- public/app.js
+++ public/app.js
@@ -1,8 +1,6 @@
 /* global bootbox */
 $(document).ready(function () {
-  // Setting a reference to the article-container div where all the dynamic content will go
-  // Adding event listeners to any dynamically generated "save article"
-  // and "scrape new article" buttons
+  
   var articleContainer = $(".article-container");
   $(document).on("click", ".btn.save", handleArticleSave);
   $(document).on("click", "#scrape-new", handleArticleScrape);
@@ -24,23 +22,15 @@ $(document).ready(function () {
 
 
   function renderArticles(articles) {
-    // This function handles appending HTML containing our article data to the page
-    // We are passed an array of JSON containing all available articles in our database
     var articleCards = [];
-    // We pass each article JSON object to the createCard function which returns a bootstrap
-    // card with our article data inside
     for (var i = 0; i < articles.length; i++) {
       articleCards.push(createCard(articles[i]));
     }
-    // Once we have all of the HTML for the articles stored in our articleCards array,
-    // append them to the articleCards container
     articleContainer.append(articleCards);
   }
 
   function createCard(article) {
-    // This function takes in a single JSON object for an article/headline
-    // It constructs a jQuery element containing all of the formatted HTML for the
-    // article card
+    
     var card = $("<div class='card'>");
     var cardHeader = $("<div class='card-header'>").append(
       $("<h3>").append(
@@ -53,16 +43,12 @@ $(document).ready(function () {
     var cardBody = $("<div class='card-body'>").text(article.summary);
 
     card.append(cardHeader, cardBody);
-    // We attach the article's id to the jQuery element
-    // We will use this when trying to figure out which article the user wants to save
     card.data("_id", article._id);
     // We return the constructed card jQuery element
     return card;
   }
 
   function renderEmpty() {
-    // This function renders some HTML to the page explaining we don't have any articles to view
-    // Using a joined array of HTML string data because it's easier to read/change than a concatenated string
     var emptyAlert = $(
       [
         "<div class='alert alert-warning text-center'>",
@@ -84,9 +70,6 @@ $(document).ready(function () {
   }
 
   function handleArticleSave() {
-    // This function is triggered when the user wants to save an article
-    // When we rendered the article initially, we attached a javascript object containing the headline id
-    // to the element using the .data method. Here we retrieve that.
     var articleToSave = $(this)
       .parents(".card")
       .data();
@@ -114,9 +97,6 @@ $(document).ready(function () {
   function handleArticleScrape() {
     // This function handles the user clicking any "scrape new article" buttons
     $.get("/api/fetch").then(function (data) {
-      // If we are able to successfully scrape the NYTIMES and compare the articles to those
-      // already in our collection, re render the articles on the page
-      // and let the user know how many unique articles we were able to save
       initPage();
       bootbox.alert($("<h3 class='text-center m-top-80'>").text(data.message));
     });
@@ -191,7 +171,6 @@ $(document).ready(function () {
     // Grab the id associated with the article from the submit button
     var thisId = $(this).attr("data-id");
 
-    // Run a POST request to change the note, using what's entered in the inputs
     $.ajax({
         method: "POST",
         url: "/articles/" + thisId,
--- server.js
+++ server.js
@@ -48,16 +48,14 @@ app.get("/all", function (req, res) {
 // A GET route for scraping the echoJS website
 app.get("/scrape", function (req, res) {
     // First, we grab the body of the html with axios
-    axios.get("https://www.reddit.com/news").then(function (response) {
+    axios.get("https://www.reddit.com").then(function (response) {
         // Then, we load that into cheerio and save it to $ for a shorthand selector
         var $ = cheerio.load(response.data);
 
-        // Now, we grab every h2 within an article tag, and do the following:
         $("h2.imors3-0").each(function (i, element) {
             // Save an empty result object
             var result = {};
 
-            // Add the text and href of every link, and save them as properties of the result object
             result.title = $(this)
                 .text();
             result.link = $(this)
@@ -96,11 +94,7 @@ app.get("/articles", function (req, res) {
 
 // Route for grabbing a specific Article by id, populate it with it's note
 app.get("/articles/:id", function (req, res) {
-    // TODO
-    // ====
-    // Finish the route so it finds one article using the req.params.id,
-    // and run the populate method with "note",
-    // then responds with the article with the note included
+    
     db.Article.findOne({ _id: req.params.id })
     .populate("note").then(function(dbArticle){
         res.json(dbArticle);
@@ -111,11 +105,7 @@ app.get("/articles/:id", function (req, res) {
 
 // Route for saving/updating an Article's associated Note
 app.post("/articles/:id", function (req, res) {
-    // TODO
-    // ====
-    // save the new note that gets posted to the Notes collection
-    // then find an article from the req.params.id
-    // and update it's "note" property with the _id of the new note
+    
     db.Note.create(req.body).then(function(dbArticle){
         res.json(dbArticle);
     }).catch(function(err) {
